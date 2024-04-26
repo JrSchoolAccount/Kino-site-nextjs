e13f3afb4bb91858cc0d8451b67b5965b724c69f
felix.hultman <karlfelixhultman@gmail.com>
2024-04-26 11:21:32 +0200

    removed from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import MainNavbar from './ui/MainNavbar';
-import TestComponent from './components/movie/testComponent';
+
 
 export default function Home() {
   return (
     <div>
       <MainNavbar />
-      <TestComponent />
+      
     </div>
   );
 }
