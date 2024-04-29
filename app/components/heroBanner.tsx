eb73788b7051cd9e550c6694a02461fd8a8c7fb5
Felix Hultman <karlfelixhultman@gmail.com>
2024-04-29 17:03:29 +0200

    added herobanner with slideshow from previous project

---
+++ app/components/heroBanner.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { styled } from '@mui/system';
+import Image from 'next/image';
+
+const SlideContainer = styled('div')({
+  display: 'none',
+  '&.active': {
+    display: 'block',
+  },
+});
+
+// get movieposters here
+const posters = [''];
+
+const HeroBanner = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlideIndex((prevIndex) => (prevIndex + 1) % posters.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      {posters.map((posters, index) => (
+        <SlideContainer
+          key={index}
+          className={index === slideIndex ? 'active' : ''}
+        >
+          <Image
+            width={500}
+            height={500}
+            src={posters}
+            alt={`Movieposter of ${index}`}
+          />
+        </SlideContainer>
+      ))}
+    </div>
+  );
+};
+
+export default HeroBanner;
