34941321c92bb4730c42920371e72d7040b4c55f
simonpizevski <78857703+simonpizevski@users.noreply.github.com>
2024-05-06 13:17:32 +0200

    removed theme

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {
   Container,
   Box,
@@ -15,114 +14,86 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#ffffff',
-    },
-    background: {
-      //darkgray
-      default: '#212121',
-    },
-    text: {
-      //white
-      primary: '#ffffff',
-      //gray
-      secondary: '#bdbdbd',
-    },
-  },
-});
-
 export default function Footer() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        bgcolor='background.default'
-        py={4}
-        px={2}
-        position='fixed'
-        bottom='0'
-        width='100%'
-      >
-        <Container maxWidth='lg'>
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={8}>
-              <Grid container spacing={4}>
-                <Grid item xs={12} md={4}>
-                  <Typography variant='h6' color='primary' gutterBottom>
-                    Öppettider
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Måndag - Fredag: xx - xx
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Lördag: xx - xx
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Söndag: xx - xx
-                  </Typography>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                  <Typography variant='h6' color='primary' gutterBottom>
-                    Adress
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Regnagården 47
-                  </Typography>
-                </Grid>
+    <Box
+      bgcolor='background.default'
+      py={4}
+      px={2}
+      position='fixed'
+      bottom='0'
+      width='100%'
+    >
+      <Container maxWidth='lg'>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={8}>
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={4}>
+                <Typography variant='h6' color='primary' gutterBottom>
+                  Öppettider
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Måndag - Fredag: xx - xx
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Lördag: xx - xx
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Söndag: xx - xx
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography variant='h6' color='primary' gutterBottom>
+                  Adress
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Regnagården 47
+                </Typography>
+              </Grid>
 
-                <Grid item xs={12} md={4}>
-                  <Typography variant='h6' color='primary' gutterBottom>
-                    Kontakt
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Email:{' '}
-                    <Link href='mailto:regna@folketsbio.se'>
-                      regna@folketsbio.se
-                    </Link>
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Tel: 0101010
-                  </Typography>
-                </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography variant='h6' color='primary' gutterBottom>
+                  Kontakt
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Email:{' '}
+                  <Link href='mailto:regna@folketsbio.se'>
+                    regna@folketsbio.se
+                  </Link>
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Tel: 0101010
+                </Typography>
               </Grid>
             </Grid>
-
-            <Grid item xs={12} md={4}>
-              <Box display='flex' flexDirection='column' alignItems='center'>
-                <Box mb={2}>
-                  <IconButton
-                    href='https://github.com/JrSchoolAccount/Kino-site-nextjs'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <GitHubIcon sx={{ color: 'text.primary' }} />
-                  </IconButton>
-                  <IconButton
-                    href='#'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <FacebookIcon sx={{ color: 'text.primary' }} />
-                  </IconButton>
-                  <IconButton
-                    href='#'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <InstagramIcon sx={{ color: 'text.primary' }} />
-                  </IconButton>
-                </Box>
-                <Box textAlign='center'>
-                  <Typography variant='body2' color='text.secondary'>
-                    © {new Date().getFullYear()} Biograf Regna AB.
-                  </Typography>
-                </Box>
-              </Box>
-            </Grid>
           </Grid>
-        </Container>
-      </Box>
-    </ThemeProvider>
+
+          <Grid item xs={12} md={4}>
+            <Box display='flex' flexDirection='column' alignItems='center'>
+              <Box mb={2}>
+                <IconButton
+                  href='https://github.com/JrSchoolAccount/Kino-site-nextjs'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <GitHubIcon sx={{ color: 'text.primary' }} />
+                </IconButton>
+                <IconButton href='#' target='_blank' rel='noopener noreferrer'>
+                  <FacebookIcon sx={{ color: 'text.primary' }} />
+                </IconButton>
+                <IconButton href='#' target='_blank' rel='noopener noreferrer'>
+                  <InstagramIcon sx={{ color: 'text.primary' }} />
+                </IconButton>
+              </Box>
+              <Box textAlign='center'>
+                <Typography variant='body2' color='text.secondary'>
+                  © {new Date().getFullYear()} Biograf Regna AB.
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 }
