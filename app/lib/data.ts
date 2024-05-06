52c987ff437f4bdb8628976613a1c4320f7acf72
FelixHultman <145000258+FelixHultman@users.noreply.github.com>
2024-05-06 19:26:00 +0200

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,21 +2,15 @@ import {
     SpecificScreeningsResponse,
 } from './definitions'
 
-
-
 export function getSpecificScreenings(id: number): SpecificScreeningsResponse {
     const screenings = [
       { id: 1, start_time: '2024-04-20T10:00:00Z' },
       { id: 2, start_time: '2024-05-26T10:00:00Z' },
       { id: 3, start_time: '2024-05-27T10:00:00Z' },
     ];
-    
     const currentTime = new Date().getTime();
     const specificScreenings = screenings.filter((screening) => {
       return new Date(screening.start_time).getTime() >= currentTime;
     });
     return { data: specificScreenings };
   }
-
-  
-
