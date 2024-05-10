a14492f50ca6f0f4c91deccf6d98b647fd69bd9a
felix.hultman <karlfelixhultman@gmail.com>
2024-05-10 13:42:04 +0200

    filmsida

---
+++ app/(pages)/filmer/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return(
+        <>
+        <h1>Filmer</h1>
+        <h2>Film 1</h2>
+        <h2>Film 2</h2>
+        <h2>Film 3</h2>
+        </>
+    );
+}
