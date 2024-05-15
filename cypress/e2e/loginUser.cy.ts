ecd754f486275f7a6df95c9c7cd9434373a378b9
JrSchoolAccount <jesperx2000@gmail.com>
2024-05-15 21:55:23 +0200

    E2E test login/signup

--- cypress/e2e/loginUser.cy.ts
+++ cypress/e2e/loginUser.cy.ts
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Sign in Form Test', () => {
+  it('should fill out the sign in form and login', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('a.MuiButton-root[href="/login"][aria-label="Logga in"]').click();
+
+    cy.get('input[name="email"]').should('be.visible');
+
+    cy.get('input[name="email"]').type('jon.doe@gmail.com');
+    cy.get('input[name="password"]').type('asdfgh');
+
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+
+    cy.url().should('include', '/profil');
   });
 });
--- cypress/e2e/registerNewUser.cy.ts
+++ cypress/e2e/registerNewUser.cy.ts
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Signup Form Test', () => {
+  it('should fill out the signup form', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get(
+      'a.MuiButton-root[href="/registrera"][aria-label="Registrera"]',
+    ).click();
+
+    cy.get('input[name="name"]').should('be.visible');
+
+    cy.get('input[name="name"]').type('Jon Doe');
+    cy.get('input[name="email"]').type('jon.doe@gmail.com');
+    cy.get('input[name="password"]').type('asdfgh');
+
+    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+
+    cy.url().should('include', '/login');
   });
 });
