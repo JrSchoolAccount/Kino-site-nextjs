96056e5d98122000a9c0ac0e9a731142471d49c7
Nelson <sherman.erik@gmail.com>
2024-05-17 14:13:47 +0200

    added component to show specific screenings

--- app/(pages)/filmer/[id]/page.tsx
+++ app/(pages)/filmer/[id]/page.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import Movie from '@/app/components/filmer/MovieDetails';
+import ScreeningsTableSpecificMovie from '@/app/ui/screeningsTableSpecificMovie';
 
-export default async function Page({
+export default function Page({
   params,
 }: {
   params: {
@@ -15,6 +16,7 @@ export default async function Page({
       </Link>
       <div>
         <Movie movieId={params.id} />
+        <ScreeningsTableSpecificMovie movie_id={params.id} />
       </div>
     </>
   );
