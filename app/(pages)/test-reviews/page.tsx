0627da94da7bdd222ec15bac83b17dcf618881a3
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-05-18 22:03:40 +0200

    created fake-file for helping test-code

---
+++ app/(pages)/test-reviews/page.tsx
@@ -0,0 +1,8 @@
+// created only this because of testing to send reviews
+import Reviews from '@/app/ui/reviews';
+
+export default function Page() {
+          return (
+                    <Reviews movieId="573a139ef29313caabcfbd6a" movieTitle="Test Movie" />
+          )
+}
