b79869265a5cdc794775a4354c6142c853ad2495
Nelson <sherman.erik@gmail.com>
2024-05-22 11:12:36 +0200

    removed unused files and code segments

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -3,14 +3,6 @@ export type SessionPayload = {
   expiresAt: Date;
   email: string;
 };
-export type Screenings = {
-  id: number;
-  start_time: string;
-};
-
-export type SpecificScreeningsResponse = {
-  data: Screenings[];
-};
 
 export type Movie = {
   id: string;
@@ -36,7 +28,6 @@ export type carouselSettings = {
   autoplaySpeed: number;
 };
 
-//This type is based on the info in our database, at some point we should fix so we don't have both Screenings and Screening as types if it doesn't fill a purpose
 export type Screening = {
   movie: string;
   saloon: string;
